37113d0811b3e4f9dfba1f3766cee678339bc437
pierremaccini@gmail.com <pierremaccini@gmail.com@7cbd00cf-3617-b532-1775-7426a6ebd87a>
2013-01-06 00:02:53 +0000

    Menus accueil + lection monde + selection de niveau Renommage Classes Lua de type director

---
+++ trunk/AquatrailGame/IceWorld.lua
@@ -0,0 +1,105 @@
+module(..., package.seeall)
+-- Main function - MUST return a display.newGroup()
+new = function ( params )
+	
+	local localGroup = display.newGroup()
+		-- Background
+	-- Create a background colour just to make the screen look a little nicer
+	local backcolor = display.newRect(0, 0, display.contentWidth, display.contentHeight)
+	backcolor:setFillColor(158, 233, 235)
+	localGroup:insert(backcolor)
+	
+	--Background
+	local back = display.newImage("fond_glace.png")
+   back.isVisible = true
+   back.x = 	display.contentWidth/2 
+   back.y =  display.contentHeight/2 
+   localGroup:insert(back)
+   
+   --return
+	local reBtn = display.newImage("bouton_retour_glace.png")
+   reBtn.isVisible = true
+   reBtn.x = reBtn.width
+   reBtn.y = display.contentHeight-display.contentHeight/4 +display.contentHeight/8
+   localGroup:insert(reBtn)	--textWorlds	local textWorlds = display.newImage("texte_glace.png")
+   textWorlds.isVisible = true
+   textWorlds.x = 	display.contentWidth/2 
+   textWorlds.y =  display.contentHeight/7
+   localGroup:insert(textWorlds)      --case1Btn	local case1Btn = display.newImage("bouton_niveau_glace.png")
+   case1Btn.isVisible = true
+   case1Btn.x = display.contentWidth/2 -case1Btn.width*1.5
+   case1Btn.y =  display.contentHeight/1.75 -case1Btn.height/1.5
+   localGroup:insert(case1Btn)    local case1Text = display.newText("1", 0, 0, native.systemFont, 32)
+	case1Text:setTextColor(87, 163, 166)	case1Text.isVisible = true	case1Text.x = case1Btn.x
+	case1Text.y = case1Btn.y      --case2Btn	local case2Btn = display.newImage("bouton_niveau_glace.png")
+   case2Btn.isVisible = true
+   case2Btn.x = display.contentWidth/2 
+   case2Btn.y =  display.contentHeight/1.75 -case2Btn.height/1.5
+   localGroup:insert(case2Btn)   local case2Text = display.newText("2", 0, 0, native.systemFont, 32)
+	case2Text:setTextColor(87, 163, 166)	case2Text.isVisible = false	case2Text.x = case2Btn.x
+	case2Text.y = case2Btn.y      --case3Btn	local case3Btn = display.newImage("bouton_niveau_glace.png")
+   case3Btn.isVisible = true
+   case3Btn.x = display.contentWidth/2 +case3Btn.width*1.5
+   case3Btn.y =  display.contentHeight/1.75 -case3Btn.height/1.5
+   localGroup:insert(case3Btn)   local case3Text = display.newText("3", 0, 0, native.systemFont, 32)
+	case3Text:setTextColor(87, 163, 166)	case3Text.isVisible = false	case3Text.x = case3Btn.x
+	case3Text.y = case3Btn.y      --case4Btn	local case4Btn = display.newImage("bouton_niveau_glace.png")
+   case4Btn.isVisible = true
+   case4Btn.x = display.contentWidth/2 -case4Btn.width*1.5
+   case4Btn.y =  display.contentHeight/1.75 +case4Btn.height/1.5
+   localGroup:insert(case4Btn)   local case4Text = display.newText("4", 0, 0, native.systemFont, 32)
+	case4Text:setTextColor(87, 163, 166)	case4Text.isVisible = false	case4Text.x = case4Btn.x
+	case4Text.y = case4Btn.y      --case5Btn	local case5Btn = display.newImage("bouton_niveau_glace.png")
+   case5Btn.isVisible = true
+   case5Btn.x = display.contentWidth/2
+   case5Btn.y =  display.contentHeight/1.75 +case5Btn.height/1.5
+   localGroup:insert(case5Btn)   local case5Text = display.newText("5", 0, 0, native.systemFont, 32)
+	case5Text:setTextColor(87, 163, 166)	case5Text.isVisible = false	case5Text.x = case5Btn.x
+	case5Text.y = case5Btn.y      --case6Btn	local case6Btn = display.newImage("bouton_niveau_glace.png")
+   case6Btn.isVisible = true
+   case6Btn.x = display.contentWidth/2 +case6Btn.width*1.5
+   case6Btn.y =  display.contentHeight/1.75 +case6Btn.height/1.5
+   localGroup:insert(case6Btn)   local case6Text = display.newText("6", 0, 0, native.systemFont, 32)
+	case6Text:setTextColor(87, 163, 166)	case6Text.isVisible = false	case6Text.x = case6Btn.x
+	case6Text.y = case6Btn.y		--cadenas   local case1Cad = display.newImage("cadenas_niveau_bloque.png")
+   case1Cad.isVisible = false
+   case1Cad.x = case1Btn.x
+   case1Cad.y =  case1Btn.y
+   localGroup:insert(case1Cad)   --   local case2Cad = display.newImage("cadenas_niveau_bloque.png")
+   case2Cad.isVisible = true
+   case2Cad.x = case2Btn.x
+   case2Cad.y =  case2Btn.y
+   localGroup:insert(case2Cad)   --   local case3Cad = display.newImage("cadenas_niveau_bloque.png")
+   case3Cad.isVisible = true
+   case3Cad.x = case3Btn.x
+   case3Cad.y =  case3Btn.y
+   localGroup:insert(case3Cad)   --   local case4Cad = display.newImage("cadenas_niveau_bloque.png")
+   case4Cad.isVisible = true
+   case4Cad.x = case4Btn.x
+   case4Cad.y =  case4Btn.y
+   localGroup:insert(case4Cad)   --   local case5Cad = display.newImage("cadenas_niveau_bloque.png")
+   case5Cad.isVisible = true
+   case5Cad.x = case5Btn.x
+   case5Cad.y =  case5Btn.y
+   localGroup:insert(case5Cad)   --   local case6Cad = display.newImage("cadenas_niveau_bloque.png")
+   case6Cad.isVisible = true
+   case6Cad.x = case6Btn.x
+   case6Cad.y =  case6Btn.y
+   localGroup:insert(case6Cad)      --Level 1 Clic
+	   	local function pressCase1(event)
+		if event.phase == "ended" then
+			director:changeScene ("Ice1")
+		end
+	end
+	case1Btn:addEventListener ("touch", pressCase1)   
+	--Return Button
+	   	local function pressReturn (event)
+		if event.phase == "ended" then
+			director:changeScene ("WorldSelection")
+		end
+	end
+	reBtn:addEventListener ("touch", pressReturn)
+
+	-- MUST return a display.newGroup()
+	return localGroup
+end
--- trunk/AquatrailGame/TitleScreen.lua
+++ trunk/AquatrailGame/TitleScreen.lua
@@ -7,52 +7,69 @@ new = function ( params )
 	-- Create a background colour just to make the screen look a little nicer
 	local backcolor = display.newRect(0, 0, display.contentWidth, display.contentHeight)
 	backcolor:setFillColor(169, 214, 255)
-	localGroup:insert(backcolor)		--Background	local back = display.newImage("fondAccueil.png")
-   back.isVisible = true   back.x = 	display.contentWidth/2 
+	localGroup:insert(backcolor)
+	
+	--Background
+	local back = display.newImage("fond_accueil.png")
+   back.isVisible = true
+   back.x = 	display.contentWidth/2 
    back.y =  display.contentHeight/2 
-   localGroup:insert(back)      --logo	local logo = display.newImage("logo_aquatrail.png")
-   logo.isVisible = true   logo.x =  display.contentWidth/2 + logo.width / 4
-   logo.y =  display.contentHeight/2 - logo.height /1.5
    localGroup:insert(back)
+   
+   --logo
+	local logo = display.newImage("logo_aquatrail.png")
+   logo.isVisible = true
+   logo.x =  display.contentWidth/2 + logo.width /4
+   logo.y =  display.contentHeight/2 - logo.height /2.5
+   localGroup:insert(logo)
 	
 	--PlayButton
   	local playBtn = display.newImage("bouton_jouer.png")
-   	playBtn.x = 	display.contentWidth/2 
-   	playBtn.y =  display.contentHeight/2 
+   	playBtn.x =  display.contentWidth/2 
+   	playBtn.y =  display.contentHeight-display.contentHeight/4-display.contentHeight/8
    	playBtn.isVisible = true
-   	localGroup:insert(playBtn)   	   	--OptionsButton
+   	localGroup:insert(playBtn)
+   	
+   	--OptionsButton
   	local OptBtn = display.newImage("bouton_options.png")
    	OptBtn.x = 	display.contentWidth/2 
-   	OptBtn.y =  display.contentHeight-display.contentHeight/4 
+   	OptBtn.y =  display.contentHeight-display.contentHeight/4 +display.contentHeight/8
    	OptBtn.isVisible = true
-   	localGroup:insert(OptBtn)   	   	--RecompensesButton
+   	localGroup:insert(OptBtn)
+   	
+   	--RecompensesButton
   	local RecBtn = display.newImage("bouton_recompenses.png")
    	RecBtn.x = 	display.contentWidth/3
-   	RecBtn.y =  display.contentHeight-display.contentHeight/4-display.contentHeight/8
+   	RecBtn.y =  display.contentHeight-display.contentHeight/4 
    	RecBtn.isVisible = true
-   	localGroup:insert(RecBtn)   	   	--ScoreButton
+   	localGroup:insert(RecBtn)
+   	
+   	--ScoreButton
   	local ScoreBtn = display.newImage("bouton_scores.png")
    	ScoreBtn.x = 	display.contentWidth-display.contentWidth/3
-   	ScoreBtn.y =    display.contentHeight-display.contentHeight/4-display.contentHeight/8
+   	ScoreBtn.y =    display.contentHeight-display.contentHeight/4 
    	ScoreBtn.isVisible = true
-   	localGroup:insert(ScoreBtn)   	   	--CreditButton
+   	localGroup:insert(ScoreBtn)
+   	
+   	--CreditButton
   	local CreditBtn = display.newImage("bouton_credits.png")
-   	CreditBtn.x = display.contentWidth - CreditBtn.width*2
-  	 CreditBtn.y = display.contentHeight - CreditBtn.height
+   	CreditBtn.x = display.contentWidth - CreditBtn.width/1.5
+  	 CreditBtn.y = display.contentHeight-display.contentHeight/4 +display.contentHeight/8
    	CreditBtn.isVisible = true
    	localGroup:insert(CreditBtn)
    
 	--Exit Button
-	local exitBtn = display.newImage("Exit_Button.png")
-  	exitBtn.x = display.contentWidth/2 
-  	exitBtn.y = display.contentHeight - exitBtn.height
+	local exitBtn = display.newImage("bouton_quitter.png")
+  	exitBtn.x = exitBtn.width
+  	 exitBtn.y = display.contentHeight-display.contentHeight/4 +display.contentHeight/8
    	exitBtn.isVisible = true
-  	localGroup:insert(exitBtn)
+  	localGroup:insert(exitBtn)
+
 	
 	--PLAY function
 	local function pressPlay (event)
 		if event.phase == "ended" then
-			director:changeScene ("Screen2")
+			director:changeScene ("WorldSelection")
 		end
 	end
 	playBtn:addEventListener ("touch", pressPlay)
