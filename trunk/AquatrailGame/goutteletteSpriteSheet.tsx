fcad06388fd2665a20436e62939dba8502132bd9
florian.olivari@gmail.com <florian.olivari@gmail.com@7cbd00cf-3617-b532-1775-7426a6ebd87a>
2013-01-20 09:02:50 +0000

    Level Design Niveau 1

--- trunk/AquatrailGame/goutteletteSpriteSheet.tsx
+++ trunk/AquatrailGame/goutteletteSpriteSheet.tsx
@@ -1,15 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="SupergoutteletteSpriteSheet" tilewidth="32" tileheight="32">
- <image source="SupergoutteletteSpriteSheet.png" width="160" height="32"/>
+<tileset name="goutteletteSpriteSheet" tilewidth="32" tileheight="32">
+ <image source="goutteletteSpriteSheet.png" width="64" height="32"/>
  <tile id="0">
   <properties>
    <property name="HasBody" value=""/>
    <property name="IsAnimated" value=""/>
    <property name="IsCollectable" value=""/>
-   <property name="Name" value="SuperGouttelette"/>
-   <property name="Nom" value="2"/>
-   <property name="Score" value="200"/>
-   <property name="animation1" value="frameCount=5,time=2000"/>
+   <property name="Name" value="Gouttelette"/>
+   <property name="Score" value="100"/>
+   <property name="animation1" value="frameCount=1,time=2000"/>
    <property name="bodyType" value="static"/>
    <property name="isSensor" value="true"/>
    <property name="sequences" value="animation1"/>
---
+++ trunk/AquatrailGame/stalactite_01.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="stalactite_01" tilewidth="32" tileheight="32">
+ <image source="stalactite_01.png" width="32" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="HasBody" value=""/>
+   <property name="IsDestructible" value=""/>
+   <property name="bodyType" value="static"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="HasBody" value=""/>
+   <property name="IsDestructible" value=""/>
+   <property name="bodyType" value="static"/>
+  </properties>
+ </tile>
+</tileset>
