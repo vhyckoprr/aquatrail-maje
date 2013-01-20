b8e41deaebbba7db0e146889a20916ad6a17f4e0
pierremaccini@gmail.com <pierremaccini@gmail.com@7cbd00cf-3617-b532-1775-7426a6ebd87a>
2013-01-20 14:02:12 +0000

    score list ok -> next : get info from server

--- trunk/AquatrailGame/DynResManager.lua
+++ trunk/AquatrailGame/DynResManager.lua
@@ -1,11 +1,50 @@
-module(..., package.seeall)--====================================================================--
+module(..., package.seeall)
+
+--====================================================================--
 -- GET Height Visible
 --====================================================================--
 
-local getScreenHeightPhysPix = function( )	local maxVisibleY = display.viewableContentHeight + -1* display.screenOriginY	local screenHeightAppPix = maxVisibleY - display.screenOriginY	local screenHeightPhysPix  = math.floor(screenHeightAppPix / display.contentScaleY + 0.51)	--print("Y"..maxVisibleY.." "..screenHeightAppPix.." "..screenHeightPhysPix)	return screenHeightPhysPixend--====================================================================--
+local getScreenHeightPhysPix = function( )
+	local maxVisibleY = display.viewableContentHeight + -1* display.screenOriginY
+	local screenHeightAppPix = maxVisibleY - display.screenOriginY
+	local screenHeightPhysPix  = math.floor(screenHeightAppPix / display.contentScaleY + 0.51)
+	--print("Y"..maxVisibleY.." "..screenHeightAppPix.." "..screenHeightPhysPix)
+	return screenHeightPhysPix
+end
+
+--====================================================================--
 -- GET Width Visible
---====================================================================--local getScreenWidthPhysPix = function( )	local maxVisibleX = display.viewableContentWidth + -1* display.screenOriginX	local screenWidthAppPix  = maxVisibleX - display.screenOriginX	local screenWidthPhysPix   = math.floor(screenWidthAppPix / display.contentScaleX + 0.51)	--print("X"..maxVisibleX.." "..screenWidthAppPix.." "..screenWidthPhysPix)	return screenWidthPhysPixend--====================================================================--
+--====================================================================--
+
+local getScreenWidthPhysPix = function( )
+	local maxVisibleX = display.viewableContentWidth + -1* display.screenOriginX
+	local screenWidthAppPix  = maxVisibleX - display.screenOriginX
+	local screenWidthPhysPix   = math.floor(screenWidthAppPix / display.contentScaleX + 0.51)
+	--print("X"..maxVisibleX.." "..screenWidthAppPix.." "..screenWidthPhysPix)
+	return screenWidthPhysPix
+end
+
+--====================================================================--
 -- Create Center Rectangle fitted
---====================================================================--local createCenterRectangleFitted = function( )		local maxVisibleX = display.viewableContentWidth + -1* display.screenOriginX	local screenWidthAppPix  = maxVisibleX - display.screenOriginX	local screenWidthPhysPix   = math.floor(screenWidthAppPix / display.contentScaleX + 0.51)		local maxVisibleY = display.viewableContentHeight + -1* display.screenOriginY	local screenHeightAppPix = maxVisibleY - display.screenOriginY	local screenHeightPhysPix  = math.floor(screenHeightAppPix / display.contentScaleY + 0.51)		local back = display.newRect(math.floor(maxVisibleX-screenWidthAppPix),math.floor(maxVisibleY-screenHeightAppPix) , screenWidthPhysPix, screenHeightPhysPix)	return backendlocal DynResManager = { getScreenHeightPhysPix = getScreenHeightPhysPix,  getScreenWidthPhysPix = getScreenWidthPhysPix, createCenterRectangleFitted = createCenterRectangleFitted}
+--====================================================================--
+
+local createCenterRectangleFitted = function( )
+	
+	local maxVisibleX = display.viewableContentWidth + -1* display.screenOriginX
+	local screenWidthAppPix  = maxVisibleX - display.screenOriginX
+	local screenWidthPhysPix   = math.floor(screenWidthAppPix / display.contentScaleX + 0.51)
+	
+	local maxVisibleY = display.viewableContentHeight + -1* display.screenOriginY
+	local screenHeightAppPix = maxVisibleY - display.screenOriginY
+	local screenHeightPhysPix  = math.floor(screenHeightAppPix / display.contentScaleY + 0.51)
+	
+	local back = display.newRect(math.floor(maxVisibleX-screenWidthAppPix),math.floor(maxVisibleY-screenHeightAppPix) , screenWidthPhysPix, screenHeightPhysPix)
+	return back
+end
+
+
+
+
+local DynResManager = { getScreenHeightPhysPix = getScreenHeightPhysPix,  getScreenWidthPhysPix = getScreenWidthPhysPix, createCenterRectangleFitted = createCenterRectangleFitted}
 
 return DynResManager
--- trunk/AquatrailGame/ScoreScreen.lua
+++ trunk/AquatrailGame/ScoreScreen.lua
@@ -2,36 +2,156 @@ module(..., package.seeall)
 -- Main function - MUST return a display.newGroup()
 new = function ( params )
 	
-	local DynResManager = require("DynResManager")
-	local worldInfos = profile.getInfos()
-	local unlockLevel1 = worldInfos.world1.level1.unlocked
-	local unlockLevel2 = worldInfos.world1.level2.unlocked
-	local unlockLevel3 = worldInfos.world1.level3.unlocked
-	local unlockLevel4 = worldInfos.world1.level4.unlocked
-	local unlockLevel5 = worldInfos.world1.level5.unlocked
-	local unlockLevel6 = worldInfos.world1.level6.unlocked
-	
+
+
 	local localGroup = display.newGroup()
-		-- Background
-	-- Create a background colour just to make the screen look a little nicer
-	local backcolor = DynResManager.createCenterRectangleFitted()
-	backcolor:setFillColor(158, 233, 235)
-	localGroup:insert(backcolor)
+
+   
+---------------------------------LIST---------------------------------------------------------------------
+--import the widget library
+local widget = require("widget")
+
+--Create a group to hold our widgets & images
+local widgetGroup = display.newGroup()
+
+-- create a gradient for the top-half of the toolbar
+local toolbarGradient = graphics.newGradient( {168, 181, 198, 255 }, {139, 157, 180, 255}, "down" )
+
+-- create toolbar to go at the top of the screen
+local titleBar = widget.newTabBar{
+	gradient = toolbarGradient,
+	bottomFill = { 117, 139, 168, 255 },
+	height = 44
+}
+titleBar.y = display.screenOriginY + titleBar.contentHeight * 0.5
+
+-- create embossed text to go on toolbar
+local titleText = display.newEmbossedText( "Scores", 0, 0, native.systemFontBold, 20 )
+titleText:setReferencePoint( display.CenterReferencePoint )
+titleText:setTextColor( 255 )
+titleText.x = display.viewableContentWidth/2
+titleText.y = titleBar.y
+
+-- create a shadow underneath the titlebar (for a nice touch)
+local shadow = display.newImage( "shadow.png" )
+shadow:setReferencePoint( display.TopLeftReferencePoint )
+shadow.x, shadow.y = 0, titleBar.y + titleBar.contentHeight * 0.5
+shadow.xScale = 480 / shadow.contentWidth
+shadow.alpha = 0.45
+
+--Text to show which item we selected
+local itemSelected = display.newText( "You selected item ", 0, 0, native.systemFontBold, 28 )
+itemSelected:setTextColor( 0 )
+itemSelected.x = display.contentWidth + itemSelected.contentWidth * 0.5
+itemSelected.y = display.contentCenterY
+widgetGroup:insert( itemSelected )
+
+--Forward reference for our back button
+local backButton
+
+--Create Table view
+local list = widget.newTableView{
+	width = 480, 
+	height = 288,
+	bottomPadding = 8,
+	maskFile = "mask-320x448.png"
+}
+list.y = titleBar.y + titleBar.contentHeight * 0.5
+
+
+--Insert widgets/images into a group
+widgetGroup:insert( list )
+widgetGroup:insert( titleBar )
+widgetGroup:insert( titleText )
+widgetGroup:insert( shadow )
+
+
+--Handle row rendering
+local function onRowRender( event )
+	local row = event.row
+	local rowGroup = event.view
+	local label = "List item "
+	local color = 0
 	
-	--Background
-	local back = display.newImage("fond_glace.png")
-   back.isVisible = true
-   back.x = 	display.contentWidth/2 
-   back.y =  display.contentHeight/2 
-   localGroup:insert(back)      local textScore = display.newText("Score", 0, 0, native.systemFont, 64)
-	textScore:setTextColor(0, 0, 0)
-	textScore.isVisible = true	textScore.x = display.contentWidth/2 
-	textScore.y = display.contentHeight/2 		--return
-	local reBtn = display.newImage("bouton_retour_glace.png")
-   reBtn.isVisible = true
-   reBtn.x = reBtn.width
-   reBtn.y = display.contentHeight-display.contentHeight/4 +display.contentHeight/8
-   localGroup:insert(reBtn)
+	--Create the row's text
+	row.textObj = display.newRetinaText( rowGroup, label .. row.index, 0, 0, native.systemFontBold, 12 )
+	row.textObj:setTextColor( color )
+	row.textObj:setReferencePoint( display.CenterLeftReferencePoint )
+	row.textObj.x, row.textObj.y = 20, rowGroup.contentHeight * 0.5
+	rowGroup:insert( row.textObj )
+	
+	--Create the row's arrow
+	--row.arrow = display.newImage( "rowArrow.png", false )
+	--row.arrow.x = rowGroup.contentWidth - row.arrow.contentWidth * 2
+	--row.arrow.y = rowGroup.contentHeight * 0.5
+	--rowGroup:insert( row.arrow )
+end
+
+--Handle the back button release event
+local function onBackRelease()
+	--Transition in the list, transition out the item selected text and the back button
+	transition.to( list, { x = 0, time = 400, transition = easing.outExpo } )
+	transition.to( itemSelected, { x = display.contentWidth + itemSelected.contentWidth * 0.5, time = 400, transition = easing.outExpo } )
+	transition.to( backButton, { x = 60, alpha = 0, time = 400, transition = easing.outQuad } )
+end
+
+--Create the back button
+backButton = widget.newButton{
+	style = "backSmall",
+	label = "Back", 
+	yOffset = - 3,
+	onRelease = onBackRelease
+}
+backButton.alpha = 0
+backButton.x = 60
+backButton.y = titleBar.y
+widgetGroup:insert( backButton )
+
+
+--Hande row touch events
+local function onRowTouch( event )
+	local row = event.row
+	local background = event.background
+	
+	if event.phase == "press" then
+		--print( "Pressed row: " .. row.index )
+		--background:setFillColor( 0, 110, 233, 255 )
+
+	elseif event.phase == "release" or event.phase == "tap" then
+		--Update the item selected text
+		--[[
+		itemSelected.text = "You selected item " .. row.index
+		
+		--Transition out the list, transition in the item selected text and the back button
+		transition.to( list, { x = - list.contentWidth, time = 400, transition = easing.outExpo } )
+		transition.to( itemSelected, { x = display.contentCenterX, time = 400, transition = easing.outExpo } )
+		transition.to( backButton, { x = 40, alpha = 1, time = 400, transition = easing.outQuad } )
+		
+		print( "Tapped and/or Released row: " .. row.index )
+		background:setFillColor( 0, 110, 233, 255 )
+		row.reRender = true
+		]]
+	end
+end
+
+
+-- insert rows into list (tableView widget)
+for i = 1, 20 do
+	list:insertRow{
+		height = 30,
+		onRender = onRowRender,
+		listener = onRowTouch
+	}
+end
+localGroup:insert(widgetGroup)
+--------------------------------------------------------------------------------------------END LIST------------------------------------------------------------------------------------------
+   
+	
+	--return
+	local backbutton = display.newImage ("backbutton.png")
+	backbutton.x = backbutton.width / 2
+	backbutton.y = backbutton.height / 2
+   localGroup:insert(backbutton)
    
   --Return Button
 	   	local function pressReturn (event)
@@ -39,7 +159,7 @@ new = function ( params )
 			director:changeScene ("IceWorld")
 		end
 	end
-	reBtn:addEventListener ("touch", pressReturn)
+	backbutton:addEventListener ("touch", pressReturn)
 
 	-- MUST return a display.newGroup()
 	return localGroup
