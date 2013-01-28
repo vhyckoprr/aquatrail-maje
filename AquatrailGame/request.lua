ddd7ae58a22b5142202c5c90e58e2a0d92c39edc
bautista.stephane <Bautista.stephane@gmail.com>
2013-01-28 10:31:52 +0000

    Optimisation de la map

--- AquatrailGame/request.lua
+++ AquatrailGame/request.lua
@@ -1,13 +1,7 @@
---require ("xml_parser")require ("ScoreScreen")	------------------		-- listener for xml request
+require ("ScoreScreen")	------------------		-- listener for xml request
 function networkListenerData(event)	if (event.isError) then
 	   print("Network error!")
    else
    	--local xml = XmlParser:ParseXmlText(event.response)		local inbox = xml:loadFile( "score.xml", system.DocumentsDirectory )   	list:displayData(inbox)
    end
-endfunction xmlValue(xmlTree, nodeName)	for i,xmlNode in pairs(xmlTree.ChildNodes) do
-       
-		if(xmlNode.Name==nodeName) then
-			return xmlNode.Value
-		end
-	end
-end
+end
