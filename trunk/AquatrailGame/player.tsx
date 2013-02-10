9bf3f7c173276c61caff979e744473d655b28644
bautista.stephane@gmail.com <bautista.stephane@gmail.com@7cbd00cf-3617-b532-1775-7426a6ebd87a>
2013-02-10 17:38:44 +0000

    Debut fin de niveau

--- trunk/AquatrailGame/player.tsx
+++ trunk/AquatrailGame/player.tsx
@@ -1,15 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="player" tilewidth="45" tileheight="58">
- <image source="SpritesHero.png" width="405" height="348"/>
+ <image source="SpritesHero.png" width="405" height="522"/>
  <tile id="0">
   <properties>
-   <property name="ignoreCulling" value="true"/>
    <property name="HasBody" value=""/>
    <property name="IsAnimated" value=""/>
    <property name="IsPlayer" value=""/>
    <property name="animDJumping" value="startFrame=6,frameCount=3,time=800"/>
+   <property name="animEndLevel" value="startFrame=73,frameCount=6,time=1000,loopCount=1"/>
    <property name="animJumpingLiq" value="startFrame=6,frameCount=3,time=800"/>
    <property name="animJumpingSol" value="startFrame=14,frameCount=3,time=800"/>
+   <property name="animStartLevel" value="startFrame=64,frameCount=6,time=1000,loopCount=1"/>
    <property name="animTransitionLiqSol" value="startFrame=29,frameCount=5,time=500,loopCount=1"/>
    <property name="animTransitionSolLiq" value="startFrame=37,frameCount=5,time=500,loopCount=1"/>
    <property name="animWalkingGaz" value=""/>
@@ -21,8 +22,9 @@
    <property name="densityLiq" value="0.5"/>
    <property name="globalJump" value="0.35"/>
    <property name="globalSpeed" value="6"/>
+   <property name="ignoreCulling" value="true"/>
    <property name="isFixedRotation" value="true"/>
-   <property name="sequences" value="[&quot;animWalkingLiq&quot;,&quot;animWalkingSol&quot;,&quot;animJumpingLiq&quot;,&quot;animJumpingSol&quot;,&quot;animTransitionLiqSol&quot;,&quot;animTransitionSolLiq&quot;,&quot;animDJumping&quot;]"/>
+   <property name="sequences" value="[&quot;animWalkingLiq&quot;,&quot;animWalkingSol&quot;,&quot;animJumpingLiq&quot;,&quot;animJumpingSol&quot;,&quot;animTransitionLiqSol&quot;,&quot;animTransitionSolLiq&quot;,&quot;animDJumping&quot;,&quot;animStartLevel&quot;,&quot;animEndLevel&quot;]"/>
   </properties>
  </tile>
 </tileset>
