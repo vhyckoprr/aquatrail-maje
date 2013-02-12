53ee03f6ea2bea3d519b18b1952752b93318b263
Bautista.stephane@gmail.com <Bautista.stephane@gmail.com@7cbd00cf-3617-b532-1775-7426a6ebd87a>
2013-02-12 08:10:21 +0000

    Réorganisation du code, et feature

--- trunk/AquatrailGame/Ice2.lua
+++ trunk/AquatrailGame/Ice2.lua
@@ -56,7 +56,7 @@ endLevel = function (self, score, time)
 			
 	end
 
-   local visual = GameLogic.createMap("Niveau_G_2.tmx", scoreText, LEVEL)
+   local visual = GameLogic.createMap("Niveau_G_1_true", scoreText, LEVEL)
 
 	
 
