3776833d1cf8d44fa88a3785788c547dabcbaca4
theo.andreoletti@gmail.com <theo.andreoletti@gmail.com@7cbd00cf-3617-b532-1775-7426a6ebd87a>
2013-02-22 10:38:07 +0000

    modifications propriétés manquantes des assets stalactruc

--- trunk/AquatrailGame/stalactite_rodeo.tsx
+++ trunk/AquatrailGame/stalactite_rodeo.tsx
@@ -1,20 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="stalactite_01_verso" tilewidth="32" tileheight="32">
+<tileset name="stalactite_rodeo" tilewidth="32" tileheight="64">
  <image source="stalactite_01_verso.png" width="32" height="64"/>
  <tile id="0">
   <properties>
    <property name="HasBody" value=""/>
    <property name="IsDestructible" value=""/>
-   <property name="IsStalacmite" value=""/>
-   <property name="IsStalactite" value=""/>
-   <property name="bodyType" value="static"/>
-   <property name="isSensor" value="false"/>
-  </properties>
- </tile>
- <tile id="1">
-  <properties>
-   <property name="HasBody" value=""/>
-   <property name="IsDestructible" value=""/>
+   <property name="IsGround" value=""/>
    <property name="IsStalacmite" value=""/>
    <property name="IsStalactite" value=""/>
    <property name="bodyType" value="static"/>
