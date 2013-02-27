5c670d14a53adb2d903078fa2244b277ad00c852
florian.olivari <florian.olivari@gmail.com>
2013-02-27 10:49:30 +0000

    FORET

---
+++ AquatrailGame/transition_terre_herbe.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="transition_terre_herbe" tilewidth="32" tileheight="32">
+ <image source="transition_terre_herbe.png" width="64" height="64"/>
+</tileset>
